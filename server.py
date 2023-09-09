b2de376d48758d7f6cbd056b2efb84b13962e116
Tanmay Gupta <tanmaygupta@Tanmays-MacBook-Air.local>
2023-09-09 18:54:38 -0400

    added inline comments in server.py file

--- server.py
+++ server.py
@@ -3,26 +3,31 @@ from flask_sqlalchemy import SQLAlchemy
 from flasgger import Swagger, swag_from
 from flask_cors import CORS
 
-
+# Initialize the Flask app
 app = Flask(__name__)
+
+# Configure the database URI and disable tracking modifications
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://spring_take_home_db_user:T1SAvfMm3pqK7soPNL9WLGfU0iqLpLlO@dpg-cjsdo8fmbdbs7399qb5g-a.oregon-postgres.render.com/spring_take_home_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SWAGGER'] = {
     'openapi': '3.0.0'
 }
+
+# Enable CORS for the app
 CORS(app)
 
+# Initialize SQLAlchemy for database interaction
 db = SQLAlchemy(app)
 
-
+# Swagger configuration
 swagger_config = {
     "headers": [],
     "specs": [
         {
             "endpoint": 'leaderboard-api-spec',
             "route": '/',
-            "rule_filter": lambda rule: True,  # all in
-            "model_filter": lambda tag: True,  # all in
+            "rule_filter": lambda rule: True,  # Include all routes
+            "model_filter": lambda tag: True,  # Include all models
         }
     ],
     "static_url_path": "/flasgger_static",
@@ -30,6 +35,7 @@ swagger_config = {
     "specs_route": "/swagger/"
 }
 
+# Swagger template configuration
 template = {
     "openapi": "3.0.0",
     "info": {
@@ -38,11 +44,13 @@ template = {
     }
 }
 
+# Swagger template file
 swagger_template_file = "openapi.json" 
 
+# Initialize Swagger with the app and configuration
 Swagger(app, config=swagger_config, template_file=swagger_template_file)
 
-
+# Define the User model
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
@@ -59,14 +67,17 @@ class User(db.Model):
             'address': self.address
         }
 
+# Create database tables within the app context
 with app.app_context():
     db.create_all()
 
+# Define route for retrieving all users
 @app.route('/users', methods=['GET'])
 def get_users():
     users = User.query.order_by(User.points.desc()).all()
     return jsonify([user.as_dict() for user in users])
 
+# Define route for retrieving a specific user by ID
 @app.route('/user/<int:id>', methods=['GET'])
 def get_user(id):
     try:
@@ -77,6 +88,7 @@ def get_user(id):
     except:
         abort(404, description="User not found")
 
+# Define route for adding a new user
 @app.route('/user', methods=['POST'])
 def add_user():
     data = request.json
@@ -111,7 +123,7 @@ def add_user():
     db.session.commit()
     return jsonify(user.as_dict()), 201
 
-
+# Define route for deleting a user by ID
 @app.route('/user/<int:id>', methods=['DELETE'])
 def delete_user(id):
     user = db.session.get(User, id)
@@ -123,6 +135,7 @@ def delete_user(id):
     db.session.commit()
     return jsonify(deleted_user), 200  # Return the deleted user data
 
+# Define route for incrementing a user's points
 @app.route('/user/<int:id>/increment', methods=['PUT'])
 def increment_point(id):
     user = db.session.get(User, id)
@@ -133,6 +146,7 @@ def increment_point(id):
     db.session.commit()
     return jsonify(user.as_dict())
 
+# Define route for decrementing a user's points
 @app.route('/user/<int:id>/decrement', methods=['PUT'])
 def decrement_point(id):
     user = db.session.get(User, id)
@@ -146,6 +160,7 @@ def decrement_point(id):
     db.session.commit()
     return jsonify(user.as_dict())
 
+# Define error handlers for 404 and 400 errors
 @app.errorhandler(404)
 def resource_not_found(e):
     return jsonify(error=str(e)), 404
@@ -154,8 +169,7 @@ def resource_not_found(e):
 def bad_request(e):
     return jsonify(error=str(e)), 400
 
+# Run the application if executed as the main script
 if __name__ == '__main__':
     from waitress import serve
     serve(app, host="0.0.0.0", port=10000)
-
-
